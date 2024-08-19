a1e84f2b15e53ef9e4e5fa39e49acba2c156b970
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-19 08:19:29 +0100

    refactor: Group query and filename variables into a Config struct
    - now the code clearly conves that query and filename are related and their purpose is to configure how the program will work

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 use std::{env, fs};
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(query: String, filename: String) -> Config {
+        Config { query, filename }
+    }
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config::new(query, filename)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("searching {}", query);
-    println!("in {}", filename);
+    println!("searching {}", config.query);
+    println!("in {}", config.filename);
 
-    let contents = fs::read_to_string("poem.txt").expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text: \n{}", contents);
 }
