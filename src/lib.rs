80baaa9f73158ebd8248d5db7b2afd4676bb5570
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-20 08:49:22 +0100

    feat: Implement 'search_case_insensitive' function and test it

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,16 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[derive(PartialEq, Debug)]
@@ -59,6 +68,21 @@ Pick three.
 Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        )
     }
 
     #[test]
